dc27e05a573f4498fcd708e7c4e54a7b5b1a7d44
Lari Unkari <lariunkari@gmail.com>
2020-12-03 22:40:23 +0200

    Cleanup on day02.py

--- modules/day02.py
+++ modules/day02.py
@@ -2,129 +2,80 @@
 Author: Lari Unkari
 """
 
-def getPolicy(input:str, log_level):
+def parsePolicyPassword(input:str, log_level):
     indexDash = input.index('-')
-    if log_level >= 2:
-        print(f"Policy dash char index: {indexDash}")
-
-    min = int(input[:indexDash])
-    if log_level >= 2:
-        print(f"Policy min: {min} parsed from {input[:indexDash]}")
-
-    indexEnd = input.index(' ')
-    if log_level >= 2:
-        print(f"Policy end char index: {indexEnd}")
-
-    max = int(input[indexDash+1:indexEnd])
-    if log_level >= 2:
-        print(f"Policy max: {max} parsed from {input[indexDash+1:indexEnd]}")
-
-    char = input[indexEnd+1:indexEnd+2]
-    if log_level >= 2:
-        print(f"Policy char: {char} parsed from {input[indexEnd+1:indexEnd+2]}")
-
-    return (min, max, char)
-
-def getPassword(input:str, log_level):
+    indexEndRule = input.index(' ')
     indexColon = input.index(':')
-    if log_level >= 2:
-        print(f"Policy colon char index: {indexColon}")
-    return input[indexColon+2:].replace('\n', "")
+    min = int(input[:indexDash])
+    max = int(input[indexDash+1:indexEndRule])
+    char = input[indexEndRule+1:indexEndRule+2]
+    password = input[indexColon+2:]
 
-def checkPasswordPart1(policy, password, log_level):
+    return (min, max, char, password)
+
+def checkPasswordPart1(entry, log_level):
     if log_level >= 1:
-        print(f"Policy limits: {policy[0]}-{policy[1]}, char: '{policy[2]}', password: '{password}' (length: {len(password)})")
+        print(f"Policy limits: {entry[0]}-{entry[1]}, char: '{entry[2]}', password: '{entry[3]}' (length: {len(entry[3])})")
 
-    count = password.count(policy[2])
-    result = policy[0] <= count and count <= policy[1]
-    if log_level >= 1:
-        print(f"Password: {password} contains {count} instances of {policy[2]}, isCorrect: {result}")
-
-    return (result, policy, password)
-
-def checkPasswordPart2(policy, password, log_level):
-    if log_level >= 1:
-        print(f"Policy positions: {policy[0]} and {policy[1]}, char: '{policy[2]}', password: '{password}' (length: {len(password)})")
-
-    indexLow = policy[0]-1
-    charLow = password[indexLow]
-    if log_level >= 3:
-        print(f"Char at index {indexLow} is '{charLow}'")
-    matchLow = password[indexLow] == policy[2]
-
-    indexHigh = policy[1]-1
-    charHigh = password[indexHigh]
-    if log_level >= 3:
-        print(f"Char at index {indexHigh} is '{charHigh}'")
-    matchHigh = password[indexHigh] == policy[2]
-
-    result = False
-    if matchLow and matchHigh:
-        if log_level >= 2:
-            print(f"Password: {password} contains instances of {policy[2]} at both required positions {policy[0]} and {policy[1]}")
-    elif matchLow:
-        result = True
-        if log_level >= 2:
-            print(f"Password: {password} contains an instance of {policy[2]} at required position {policy[0]}")
-    elif matchHigh:
-        result = True
-        if log_level >= 2:
-            print(f"Password: {password} contains an instance of {policy[2]} at required position {policy[1]}")
-    else:
-        if log_level >= 2:
-            print(f"Password: {password} contains NO instances of {policy[2]} at required positions {policy[0]} and {policy[1]}")
+    count = entry[3].count(entry[2])
+    result = entry[0] <= count and count <= entry[1]
 
     if log_level >= 1:
-        print(f"Password: {password} isCorrect: {result}")
+        print(f"Password: {entry[3]} contains {count} instances of {entry[2]}, isCorrect: {result}")
 
-    return (result, policy, password)
+    return result
+
+def checkPasswordPart2(entry, log_level):
+    if log_level >= 1:
+        print(f"Policy positions: {entry[0]} and {entry[1]}, char: '{entry[2]}', password: '{entry[3]}' (length: {len(entry[3])})")
+
+    matchLow = entry[3][entry[0]-1] == entry[2]
+    matchHigh = entry[3][entry[1]-1] == entry[2]
+    result = (matchLow and not(matchHigh)) or (matchHigh and not(matchLow))
+
+    if log_level >= 1:
+        print(f"Password: {entry[3]}, low({entry[0]-1}): {matchLow}, high({entry[1]-1}): {matchHigh}, isCorrect: {result}")
+
+    return result
 
 def checkPasswords(entries, part_num, log_level):
     countValid = 0
     countProcessed = 0
 
-    result = (0, 1, 2, 3)
+    result = False
     for index, entry in enumerate(entries):
 
         if part_num == 1:
-            result = checkPasswordPart1(entry[0], entry[1], log_level)
+            result = checkPasswordPart1(entry, log_level)
         else:
-            result = checkPasswordPart2(entry[0], entry[1], log_level)
+            result = checkPasswordPart2(entry, log_level)
 
         countProcessed += 1
 
-        if result[0]:
+        if result:
             countValid += 1
 
             if log_level >= 1:
-                print(f"Input {index} password '{result[2]}' is valid, count at {countValid}\n")
+                print(f"Input {index} password '{entry[3]}' is valid, count at {countValid}\n")
         else:
             if log_level >= 1:
-                print(f"Input {index} password '{result[2]}' is NOT valid\n")
+                print(f"Input {index} password '{entry[3]}' is NOT valid\n")
 
     print(f"Found a total of {countValid} valid passwords out of {countProcessed}")
 
-def processLine(input:str, log_level):
-    policy = getPolicy(input, log_level)
-    password = getPassword(input, log_level)
-    return (policy, password)
-
-def processInput(input_file, log_level):
-    entries = []
-    for index, input in enumerate(input_file):
-        if log_level >= 1:
-            print(f"Input string {index}: '{input[:len(input)-1]}'")
-        entries.append(processLine(input, log_level))
-
-    return entries
 
 def play(input_file, input_parameters, log_level):
     
 
     #Initialize and read input
 
-
-    entries = processInput(input_file, log_level)
+    
+    entries = []
+    for index, input in enumerate(input_file):
+        input = input.replace('\n', '')
+        if log_level >= 1:
+            print(f"Input string {index}: '{input}'")
+        entries.append(parsePolicyPassword(input, log_level))
 
 
     #Part 1 of Day 2
