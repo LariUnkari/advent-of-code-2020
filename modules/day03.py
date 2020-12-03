b0f7d84b021d95841d45db54b9b311b837a879d3
Lari Unkari <lariunkari@gmail.com>
2020-12-03 23:55:42 +0200

    Day 3 part 2 done

--- modules/day03.py
+++ modules/day03.py
@@ -2,26 +2,24 @@
 Author: Lari Unkari
 """
 
-START_SLOPE = 3
+SLOPES = [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
 def walkMap(map, startRow, startColumn, slope, log_level):
     width = len(map[0])
     height = len(map)
 
-    if log_level >= 1:
-        print(f"Map dimensions: '{width}x{height}'")
-
     countTrees = 0
 
     c = startColumn
-    for r in range(startRow, len(map)):
+    for r in range(startRow + slope[1], len(map), slope[1]):
         if log_level >= 1:
             print(f"Map[{c},{r}]: '{map[r][c]}'")
         if map[r][c] == '#':
             countTrees += 1
 
-        c = (c + slope) % width
-
+        c = (c + slope[0]) % width
+        
+    print(f"Trees found on slope {slope}: {countTrees}")
     return countTrees
 
 def play(input_file, input_parameters, log_level):
@@ -31,20 +29,25 @@ def play(input_file, input_parameters, log_level):
 
 
     map = []
-    for index, input in enumerate(input_file):
-        map.append(input.replace('\n', ''))
+    for index, inputText in enumerate(input_file):
+        map.append(inputText.replace('\n', ''))
         if log_level >= 1:
             print(f"Input string {index}: '{map[index]}'")
 
-
-    #Part 1 of Day 1
-
+    part_num = -1
+    while part_num < 1:
+        txt = input("Which part to run? 1-2 (defaults to 2): ")
+        try:
+            part_num = int(txt)
+        except ValueError:
+            print(f"Invalid input {txt} given!")
     
-    countTrees = walkMap(map, 1, START_SLOPE, START_SLOPE, log_level)
-    print(f"Trees found: {countTrees}")
-
-
-    #Part 2 of Day 1
-
-
-    
+    if part_num == 1:
+        countTrees = walkMap(map, 0, SLOPES[1][0], SLOPES[1], log_level)
+    else:
+        countTrees = 0
+        treeCountProduct = 1
+        for slope in SLOPES:
+            countTrees = walkMap(map, 0, slope[0], slope, log_level)
+            treeCountProduct *= countTrees
+        print(f"Trees count total product is {treeCountProduct}")
