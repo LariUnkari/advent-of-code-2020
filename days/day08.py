a0b9eaf7ad6de1f8d352da7049a826e512080a1a
Lari Unkari <lariunkari@gmail.com>
2020-12-08 11:26:29 +0200

    Day 8 part 2 done

--- days/day08.py
+++ days/day08.py
@@ -5,48 +5,82 @@ Author: Lari Unkari
 import io, re, modules.userInput
 
 
-
-
-
-def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
-    
-    # Initialize and read input
-
-    program = [(command[:3], int(command[4:].replace('+',''))) for command in input_stream.read().split('\n') if len(command) > 3]
-    if log_level >= 1:
-        for cmd in program: print(f"Found command: {cmd[0]} args: {cmd[1]}")
-
-    # Select which part of day to run
-    
-    #part_input = modules.userInput.get_int_input_constrained("Which part to run? 1-2 (defaults to 2): ", 1, 2, 2)
-
-    # Run
-
+def run(cmds, args, log_level):
     i = 0
     acc = 0
     visited = {}
     
-    cmd:str
-    arg:int
-    while i >= 0 and i < len(program):
+    c:str
+    a:int
+    while i >= 0 and i < len(cmds):
         if i in visited:
             print(f"Revisiting line {i}, breaking out of loop with acc: {acc}")
-            break
+            return (-1, acc)
 
         visited[i] = True
-        cmd = program[i][0]
-        arg = program[i][1]
+        c = cmds[i]
+        a = args[i]
 
-        if cmd == "jmp":
-            if log_level >= 1: print(f"[{i}] Jumping by arg: {arg} to line: {i+arg}")
-            i += arg
+        if c == "jmp":
+            if log_level >= 1: print(f"[{i}] Jumping by arg: {a} to line: {i+a}")
+            i += a
         else:
-            if cmd == "nop":
-                if log_level >= 1: print(f"[{i}] No operation, arg: {arg}")
+            if c == "nop":
+                if log_level >= 1: print(f"[{i}] No operation, arg: {a}")
             else:
-                acc += arg
-                if log_level >= 1: print(f"[{i}] Accumulate, arg: {arg}, acc: {acc}")
+                acc += a
+                if log_level >= 1: print(f"[{i}] Accumulate, arg: {a}, acc: {acc}")
 
             i += 1
 
     print(f"\nProgram terminated with value {acc} in the accumulator")
+    return (0, acc)
+
+def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
+    
+    # Initialize and read input
+    
+    cmds = []
+    args = []
+    for line in input_stream.read().split('\n'):
+        if len(line) < 3:
+            continue
+        
+        if log_level >= 2: print(f"Found command: {line[:3]} args: {line[4:]}")
+
+        cmds.append(line[:3])
+        args.append(int(line[4:].replace('+','')))
+
+    # Select which part of day to run
+    
+    part_input = modules.userInput.get_int_input_constrained("Which part to run? 1-2 (defaults to 2): ", 1, 2, 2)
+    if part_input[1] == 1:
+        run(cmds, args, log_level)
+    else:
+        lineToEdit:int = -1
+        cmdToSet:str = ""
+        newCmds = cmds.copy()
+
+        # BRUTE FORCE, ENGAGE!
+        retVal = None
+        for index, cmd in enumerate(newCmds):
+            if cmd == "jmp":
+                newCmds[index] = "nop"
+            elif cmd == "nop":
+                newCmds[index] = "jmp"
+                
+            if log_level >= 1: print(f"Replaced command {cmd} at line {index} with {newCmds[index]}")
+            retVal = run(newCmds, args, log_level - 1)
+            if retVal[0] == 0:
+                lineToEdit = index
+                cmdToSet = newCmds[index]
+                print(f"Replacing line {lineToEdit} command with {cmdToSet} allowed program to terminate")
+                break
+            
+            newCmds[index] = cmd
+            retVal = None
+
+        print(f"Final value in accumulator with fixed program: {retVal[1]}")
+        cmds[lineToEdit] = cmdToSet
+
+    # Run
