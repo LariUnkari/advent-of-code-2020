b53f8d22bd8fcf16b069563c8b143a6352e67c1f
Lari Unkari <lariunkari@gmail.com>
2020-12-10 11:51:51 +0200

    WIP Day 10 part 2, need to remove path tracking and just increment count instead

--- days/day10.py
+++ days/day10.py
@@ -8,6 +8,31 @@ DIFF_MIN = 1
 DIFF_MAX = 3
 DEVICE_DIFF = 3
 
+def find_arrangements(adapters, index, path, foundPaths, log_level):
+    path.append(index)
+
+    if index == len(adapters) - 1:
+        if log_level >= 1:
+            valPath = [adapters[i] for i in path]
+            print(f"Found a viable path: {path}\n   Values: {valPath}")
+        foundPaths.append(path)
+        return
+
+    for i in range(index + 1, len(adapters)):
+        a = adapters[index]
+        b = adapters[i]
+        d = b-a
+
+        if d < DIFF_MIN or d > DIFF_MAX:
+            if log_level >= 3: print(f"Invalid next adapter[{i}] rating {b}, previous adapter[{index}] {a}, expecting diff {d} in range {DIFF_MIN}-{DIFF_MAX}, both inclusive")
+            break
+
+        if log_level >= 2:
+            valPath = [adapters[p] for p in path]
+            print(f"Found new branch for adapter arrangement {path} -> {i}\n   Values: {valPath} -> {b}")
+
+        find_arrangements(adapters, i, path.copy(), foundPaths, log_level)
+
 def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
     
     # Initialize and read input
@@ -20,34 +45,45 @@ def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
         if log_level >= 1: print(f"Found adapter {len(adapters)}, rated for {line} jolts")
         adapters.append(int(line))
 
-    # Run
-
     adapters.sort()
     deviceRating = adapters[len(adapters) - 1] + DEVICE_DIFF
     adapters.append(deviceRating)
 
-    diffCounts = {}
-    for i in range(DIFF_MIN, DIFF_MAX + 1):
-        diffCounts[i] = 0
-
-    allDiffs = []
+    # Select which part of day to run
     
-    ratingPrevious = 0
-    for a in adapters:
-        d = a-ratingPrevious
-        if d < DIFF_MIN or d > DIFF_MAX:
-            print(f"Invalid adapter rating {a}, previous {ratingPrevious}, expecting diff {d} in range {DIFF_MIN}-{DIFF_MAX}, both inclusive")
-            return
-        
-        if log_level >= 1: print(f"Adapter rating {a}, previous {ratingPrevious}, diff {d}")
-        allDiffs.append(d)
-        
-        
-        diffCounts[d] += 1
-        ratingPrevious = a
-    
-    for c in diffCounts.keys():
-        print(f"Found {diffCounts[c]} diffs of {c}")
+    part_input = modules.userInput.get_int_input_constrained("Which part to run? 1-2 (defaults to 2): ", 1, 2, 2)
 
-    prod = diffCounts[1] * diffCounts[3]
-    print(f"Final device rating is {deviceRating}, product of diff counts 1 ({diffCounts[1]}) 3 ({diffCounts[3]}) is {prod}")
+    # Run
+
+    if part_input[1] == 1:
+        diffCounts = {}
+        for i in range(DIFF_MIN, DIFF_MAX + 1):
+            diffCounts[i] = 0
+
+        allDiffs = []
+    
+        ratingPrevious = 0
+        for a in adapters:
+            d = a-ratingPrevious
+            if d < DIFF_MIN or d > DIFF_MAX:
+                print(f"Invalid adapter rating {a}, previous {ratingPrevious}, expecting diff {d} in range {DIFF_MIN}-{DIFF_MAX}, both inclusive")
+                return
+        
+            if log_level >= 1: print(f"Adapter rating {a}, previous {ratingPrevious}, diff {d}")
+            allDiffs.append(d)
+        
+        
+            diffCounts[d] += 1
+            ratingPrevious = a
+    
+        for c in diffCounts.keys():
+            print(f"Found {diffCounts[c]} diffs of {c}")
+
+        prod = diffCounts[1] * diffCounts[3]
+        print(f"Final device rating is {deviceRating}, product of diff counts 1 ({diffCounts[1]}) 3 ({diffCounts[3]}) is {prod}")
+    else:
+        foundPaths = []
+        adapters.insert(0, 0)
+        find_arrangements(adapters, 0, [], foundPaths, log_level)
+
+        print(f"Found {len(foundPaths)} valid arrangements")
