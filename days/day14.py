c2708ba862e3757842fa26309f3757410958c386
Lari Unkari <lariunkari@gmail.com>
2020-12-14 23:28:29 +0200

    Day 14 part 2 done

--- days/day14.py
+++ days/day14.py
@@ -2,9 +2,28 @@
 Author: Lari Unkari
 """
 
-import io, math, re, modules.userInput
+import io, itertools, math, re, modules.userInput
 
 MASK_LENGTH = 36
+
+def parse_binary(binary, name, log_level):
+    s = "".join(binary)
+    val = int(s, 2)
+
+    if log_level >= 2:
+        print(f"Parsed {name} {val} from binary {s}")
+
+    return val
+
+def write_to_memory(memory, pos, val, log_level):
+    if log_level >= 1:
+        if pos in memory:
+            print(f"Mem[{pos}] set to {val}, was {memory[pos]}")
+        else:
+            print(f"Mem[{pos}] set to {val}, was not set")
+
+    memory[pos] = val
+
 
 def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
     
@@ -24,61 +43,92 @@ def play(input_stream:io.TextIOWrapper, input_parameters, log_level):
         if parts[0] == "mask":
             pos = None
             val = parts[1]
-            if log_level >= 2: print(f"Operation {len(instructions)} is '{cmd}' = '{val}'")
+            if log_level >= 2: print(f"Instruction {len(instructions)} is '{cmd}' = '{val}'")
         else:
             cmd = parts[0][:3]
             pos = int(parts[0][4:-1])
             val = int(parts[1])
-            if log_level >= 2: print(f"Operation {len(instructions)} is '{cmd}[{pos}]' = '{val}'")
+            if log_level >= 2: print(f"Instruction {len(instructions)} is '{cmd}[{pos}]' = '{val}'")
         
         instructions.append((cmd, pos, val))
 
     if log_level >= 1: print(f"Found {len(instructions)} instructions\n")
+
+    # Select which part of day to run
+    
+    part_input = modules.userInput.get_int_input_constrained("Which part to run? 1-2 (defaults to 2): ", 1, 2, 2)
 
     # Run
 
     mask = ["X"] * MASK_LENGTH
     binary = ["0"] * MASK_LENGTH
     memory = {}
+
+    floats = []
     s:str
+    address:int
     for index, instr in enumerate(instructions):
         cmd = instr[0]
+        pos = instr[1]
+        val = instr[2]
+        
 
         if cmd == "mask":
-            val = instr[2]
-
             if log_level >= 1:
                 s = "".join(mask)
-                print(f"[{index}] Mask set to '{val}'\n[{index}]    Mask was '{s}'")
+                print(f"[{index}] mask = '{val}'")
 
             for i in range(MASK_LENGTH):
                 mask[i] = val[i]
         else:
-            s = "{0:b}".format(instr[2])
-
-            if log_level >= 2: print(f"Input value {instr[2]}, binary {s}")
+            if log_level >= 1: print(f"[{index}] '{cmd}'[{pos}] = '{val}'")
+            
+            if part_input[1] == 1:
+                s = "{0:b}".format(val)
+                if log_level >= 2: print(f"Input value {val}, binary {s}")
+            else:
+                s = "{0:b}".format(pos)
+                if log_level >= 2: print(f"Address value {pos}, binary {s}")
 
             for i in range(MASK_LENGTH):
-                pos = -1-i
-                if mask[pos] != "X":
-                    binary[pos] = mask[pos]
-                elif i < len(s):
-                    binary[pos] = s[pos]
-                else:
-                    binary[pos] = "0"
-            
-            pos = instr[1]
-            s = "".join(binary)
-            val = int(s, 2)
+                address = -1-i
 
-            if log_level >= 2: print(f"Final input value {val}, binary {s}")
-            if log_level >= 1:
-                if pos in memory:
-                    print(f"[{index}] Mem[{pos}] set to {val}, was {memory[pos]}")
+                if part_input[1] == 1:
+                    if mask[address] != "X":
+                        binary[address] = mask[address]
+                    elif i < len(s):
+                        binary[address] = s[address]
+                    else:
+                        binary[address] = "0"
                 else:
-                    print(f"[{index}] Mem[{pos}] set to {val}, was not set")
+                    if mask[address] == "X":
+                        binary[address] = "X"
+                        floats.append(i)
+                    elif mask[address] == "1":
+                        binary[address] = mask[address]
+                    elif i < len(s):
+                        binary[address] = s[address]
+                    else:
+                        binary[address] = "0"
 
-            memory[pos] = val
+            if part_input[1] == 1:
+                write_to_memory(memory, pos, parse_binary(binary, "value", log_level), log_level)
+            else:
+                if len(floats) > 0:
+                    if log_level >= 2: print(f"Found {len(floats)} floats: {floats}")
+                    for p in itertools.product(range(2), repeat=len(floats)):
+                        for i in range(len(floats)):
+                            address = -1-floats[i]
+                            binary[address] = str(p[len(floats)-1-i])
+                        
+                        address = parse_binary(binary, "address permutation {0}".format(p), log_level)
+                        write_to_memory(memory, address, val, log_level)
+                    
+                    floats.clear()
+                else:
+                    address = parse_binary(binary, "address", log_level)
+                    write_to_memory(memory, address, val, log_level)
+
 
     print(f"\nDone processing instructions, memory has {len(memory)} entries")
 
