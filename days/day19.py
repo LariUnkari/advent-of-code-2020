7bd15cb113fd9116e58bc7033f6f64a131763b9c
Lari Unkari <lariunkari@gmail.com>
2020-12-20 16:19:03 +0200

    Day 19 part 2 done

--- days/day19.py
+++ days/day19.py
@@ -44,7 +44,8 @@ def check_rule(rules, key, path, log_level):
             new = "".join([o[0] for o in opts])
             strings.append(new)
 
-        if log_level >= 2: print(f"String options found: {strings}")
+        if log_level >= 2:
+           print(f"'{key}' Found {len(strings)} string options found in path: {path} + {r}\n   {strings}")
 
     return strings
 
@@ -80,13 +81,120 @@ def play(input_stream:io.TextIOWrapper, day_part, input_parameters, log_level):
 
     # Run
 
-    validMessages = {}
-    for msg in build_valid(rules, '0', log_level):
-        validMessages[msg] = True
-
     count = 0
-    for msg in messages:
-        if msg in validMessages:
+
+    if day_part == 1:
+        validMessages = {}
+
+        for msg in build_valid(rules, '0', log_level):
+            validMessages[msg] = True
+
+        for msg in messages:
+            if msg in validMessages:
+                count += 1
+    else:
+        rules['8'][1].append(['42', '8'])
+        rules['11'][1].append(['42', '11', '31'])
+
+        rules_repeating = ['42', '31']
+        validSequences = []
+        sequences:list
+
+        for i, r in enumerate(rules_repeating):
+            validSequences.append({})
+            sequences = build_valid(rules, r, log_level)
+
+            for seq in sequences:
+                validSequences[i][seq] = True
+
+            if log_level >= 1:
+                lengths = {}
+
+                # Test for sequence lengths
+                for seq in sequences:
+                    if len(seq) not in lengths:
+                        lengths[len(seq)] = True
+
+                print(f"Rule[{i}] {r} results in {len(sequences)} of lengths: {list(lengths.keys())}\n{sequences}")
+            else:
+                print(f"Rule[{i}] {r} results in {len(sequences)}:\n{sequences}")
+
+        for key in validSequences[0].keys():
+            if key in validSequences[1]:
+                print(f"ERROR: Matching sequence rules_repeating[0]={key} in both rules {rules_repeating}")
+                
+
+        # Magic number. Through testing, all permutations of given rules result in 8-char sequences.
+        # I had an extra validation loop checking all lengths found but it was ugly and pointless due to the knowledge
+        length = 8
+
+        part:str
+        seq:str
+        num:int
+        chk:int
+        path = []
+
+        for msg in messages:
+            path.clear()
+
+            if len(msg) % length != 0:
+                if log_level >= 1: print(f"Message '{msg}' failed validation of length divisble by {length}")
+                continue
+
+            part = msg
+            num = 0
+
+            # The rule '11' means the end must match '31' and number of matches of '31' from
+            # the right to left must be matched by an equal number of matches of '42', and
+            # then any multiple of '42' matches as dicated by rule '8' all the way to the to the left
+
+            while True:
+                seq = part[-length:]
+                if seq in validSequences[1]:
+                    path.append(rules_repeating[1])
+                    part = part[:-length]
+                    num += 1
+                else: break
+
+            if num == 0:
+                if log_level >= 1: print(f"Message '{msg}' failed validation of trailing sequences of {rules_repeating[1]}={rules[rules_repeating[1]][1]} as required by '11'={rules['11'][1]}")
+                continue
+            else:
+                if log_level >= 2: print(f"Message '{msg}' validated {num} trailing sequences of {rules_repeating[1]}={rules[rules_repeating[1]][1]}")
+
+            chk = num
+            while num > 0:
+                seq = part[-length:]
+                if seq in validSequences[0]:
+                    path.append(rules_repeating[0])
+                    part = part[:-length]
+                    num -= 1
+                else: break
+
+            if num > 0:
+                if log_level >= 1: print(f"Message '{msg}' failed validation of {chk} leading sequences of {rules_repeating[0]}={rules[rules_repeating[0]][1]} as required by '11'={rules['11'][1]}")
+                continue
+            else:
+                if log_level >= 2: print(f"Message '{msg}' validated {chk} leading sequences of {rules_repeating[0]}={rules[rules_repeating[0]][1]} as required by '11'={rules['11'][1]}")
+
+            chk = 0
+            while True:
+                seq = part[-length:]
+                if seq in validSequences[0]:
+                    path.append(rules_repeating[0])
+                    part = part[:-length]
+                    chk += 1
+                else: break
+
+            if chk == 0:
+                if log_level >= 1: print(f"Message '{msg}' failed validation of successive sequences of {rules_repeating[0]}={rules[rules_repeating[0]][1]} as required by '8'={rules['8'][1]}")
+                continue
+
+            if len(part) > 0:
+                if log_level >= 1: print(f"Message '{msg}' failed validation of all remaining sequences of {rules_repeating[0]}={rules[rules_repeating[0]][1]} as required by '8'={rules['8'][1]}")
+                continue
+
+            if log_level >= 1: print(f"Message '{msg}' validated with sequences: {list(reversed(path))}")
             count += 1
 
     print(f"Found {count} valid messages")
